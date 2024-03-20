50af2152b2fd0baa8fff982457571aaba69c4b01
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-03-20 21:08:12 +0530

    make order card red dynamically

--- src/components/orderCard.jsx
+++ src/components/orderCard.jsx
@@ -3,8 +3,9 @@
 const OrderCard = (props) => {
     const { order: { id, type, status, base, createdAt, timeDiff } } = props
     console.log(props)
+
     return (
-        <div class="w-44 h-36 border border-gray-300 flex flex-col justify-center text-center rounded-2xl">
+        <div className={`w-44 h-36 border border-gray-300 flex flex-col justify-center text-center rounded-2xl ${timeDiff?.minutes > 3 ? 'bg-red-200' : ''}`}>
             <div>{id}</div>
             <div>{timeDiff?.minutes} min {timeDiff?.seconds} sec</div>
             <div> <button>Next</button></div>
