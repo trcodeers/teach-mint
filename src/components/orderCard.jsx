279d2cc93c609c51843c56a9d4b2bbd98c4247f2
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-03-20 21:08:38 +0530

    red color on card

--- src/components/orderCard.jsx
+++ src/components/orderCard.jsx
@@ -5,7 +5,7 @@ const OrderCard = (props) => {
     console.log(props)
 
     return (
-        <div className={`w-44 h-36 border border-gray-300 flex flex-col justify-center text-center rounded-2xl ${timeDiff?.minutes > 3 ? 'bg-red-200' : ''}`}>
+        <div className={`w-44 h-36 border border-gray-300 flex flex-col justify-center text-center rounded-2xl ${timeDiff?.minutes > 3 ? 'bg-red-400' : ''}`}>
             <div>{id}</div>
             <div>{timeDiff?.minutes} min {timeDiff?.seconds} sec</div>
             <div> <button>Next</button></div>
