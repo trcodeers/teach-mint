b87d549f6794a3b34b7517317143b921f386098b
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-03-20 20:08:05 +0530

    order card with time

--- src/components/orderCard.jsx
+++ src/components/orderCard.jsx
@@ -1,11 +1,12 @@
 
 
 const OrderCard = (props) => {
-    const { order: { id, type, status, base, createdAt, } } = props
+    const { order: { id, type, status, base, createdAt, timeDiff } } = props
+    console.log(timeDiff)
     return (
         <div class="w-44 h-36 border border-gray-300 flex flex-col justify-center text-center rounded-2xl">
             <div>{id}</div>
-            <div>{createdAt}</div>
+            <div>{timeDiff.minutes}</div>
             <div> <button>Next</button></div>
         </div>
     )
