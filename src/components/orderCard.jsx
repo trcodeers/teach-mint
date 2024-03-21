6f8e3c2cb01a3de28a6e1c1a6f0619728fe1b030
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-03-21 20:49:29 +0530

    display time for pocked orders also

--- src/components/orderCard.jsx
+++ src/components/orderCard.jsx
@@ -7,15 +7,12 @@ const OrderCard = (props) => {
     const [timeDiffMessage, setTimeDiffMessage] = useState(null)
 
     useEffect(()=>{ 
-        if(status !== 'picked'){
             const intervalId = setInterval(() => {
                 const { minutes, seconds } = getTimeDifference(order[`${status}At`], Date.now())
                 setTimeDiffMessage({minutes, seconds})    
             }, 1000); 
           
-            return () => clearInterval(intervalId); // Cleanup on component unmount
-        }       
-    
+            return () => clearInterval(intervalId); // Cleanup on component unmount    
     },[])
 
     function getTimeDifference(startTimeStamp, endTimeStamp) {
@@ -36,7 +33,7 @@ const OrderCard = (props) => {
     return (
         <div className={`w-max-44 px-8 h-36 border border-gray-500 flex flex-col justify-center text-center rounded-2xl ${timeDiffMessage?.minutes >= 3 ? 'bg-red-400' : ''}`}>
             <div>{id}</div>
-            {timeDiffMessage && status !== 'picked' && <div>{timeDiffMessage?.minutes} min {timeDiffMessage?.seconds} sec</div>}
+            {timeDiffMessage && <div>{timeDiffMessage?.minutes} min {timeDiffMessage?.seconds} sec</div>}
                 <div> 
                     <button onClick={() => onClickNext(order)} className="bg-gray-200 px-4 py-1 mt-2 rounded-md border border-gray-500">
                         {status === 'picked' ? 'Picked' : 'Next'}
