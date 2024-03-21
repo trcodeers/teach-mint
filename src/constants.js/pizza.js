3d97629b02613b451b06afc3c025524c66880f17
Tarun-kumar-singh <tarunks2512@gmail.com>
2024-03-21 13:39:38 +0530

    constant

---
+++ src/constants.js/pizza.js
@@ -0,0 +1,12 @@
+
+export const Pizza_Type = [
+    { label: '', value : ''}
+]
+
+export const Pizza_Size = [
+    { label: '', value : ''}
+]
+
+export const Pizza_Base = [
+    { label: '', value : ''}
+]
